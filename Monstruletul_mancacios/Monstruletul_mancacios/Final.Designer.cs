552e6666865522f8bbbe83f627a1ebca396bc97f
anamariagr23 <68443472+anamariagr23@users.noreply.github.com>
2021-05-07 20:08:02 +0300

    Add files via upload

---
+++ Monstruletul_mancacios/Monstruletul_mancacios/Final.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Monstruletul_mancacios
+{
+    partial class Final
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.restart = new System.Windows.Forms.PictureBox();
+            this.iesire = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.record = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.restart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iesire)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Monstruletul_mancacios.Properties.Resources.final;
+            this.pictureBox1.Location = new System.Drawing.Point(206, 285);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 228);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::Monstruletul_mancacios.Properties.Resources.record;
+            this.pictureBox2.Location = new System.Drawing.Point(138, 193);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(149, 51);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::Monstruletul_mancacios.Properties.Resources.pierdut;
+            this.pictureBox3.Location = new System.Drawing.Point(169, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(339, 82);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = global::Monstruletul_mancacios.Properties.Resources.scor;
+            this.pictureBox4.Location = new System.Drawing.Point(138, 137);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(203, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // restart
+            // 
+            this.restart.BackColor = System.Drawing.Color.Transparent;
+            this.restart.Image = global::Monstruletul_mancacios.Properties.Resources.restart;
+            this.restart.Location = new System.Drawing.Point(244, 519);
+            this.restart.Name = "restart";
+            this.restart.Size = new System.Drawing.Size(179, 48);
+            this.restart.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.restart.TabIndex = 4;
+            this.restart.TabStop = false;
+            this.restart.Click += new System.EventHandler(this.restart_Click);
+            // 
+            // iesire
+            // 
+            this.iesire.BackColor = System.Drawing.Color.Transparent;
+            this.iesire.Image = global::Monstruletul_mancacios.Properties.Resources.iesire;
+            this.iesire.Location = new System.Drawing.Point(267, 573);
+            this.iesire.Name = "iesire";
+            this.iesire.Size = new System.Drawing.Size(135, 63);
+            this.iesire.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.iesire.TabIndex = 5;
+            this.iesire.TabStop = false;
+            this.iesire.Click += new System.EventHandler(this.iesire_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(347, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 39);
+            this.label1.TabIndex = 6;
+            // 
+            // record
+            // 
+            this.record.AutoSize = true;
+            this.record.Location = new System.Drawing.Point(310, 214);
+            this.record.Name = "record";
+            this.record.Size = new System.Drawing.Size(46, 17);
+            this.record.TabIndex = 7;
+            this.record.Text = "label2";
+            // 
+            // Final
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Monstruletul_mancacios.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(682, 653);
+            this.Controls.Add(this.record);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.iesire);
+            this.Controls.Add(this.restart);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Final";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Final";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.restart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iesire)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox restart;
+        private System.Windows.Forms.PictureBox iesire;
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label record;
+    }
+}
---
+++ Monstruletul_mancacios/Monstruletul_mancacios/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Windows.Forms;
+
+namespace Monstruletul_mancacios
+{
+    public partial class Form1 : Form
+    {
+        #region .. Double Buffered function ..
+        public static void SetDoubleBuffered(Control c)
+        {
+            if (SystemInformation.TerminalServerSession)
+                return;
+            System.Reflection.PropertyInfo aProp = typeof(Control).GetProperty("DoubleBuffered", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            aProp.SetValue(c, true, null);
+        }
+
+        #endregion
+
+        #region .. code for Flickering ..
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x02000000;
+                return cp;
+            }
+        }
+
+        #endregion
+        SoundPlayer sunet = new SoundPlayer(Properties.Resources.ra);
+
+        public Form1()
+        {
+            InitializeComponent();
+            background.Controls.Add(player);
+            player.BackColor = Color.Transparent;
+            background.Controls.Add(candy);
+            candy.BackColor = Color.Transparent;
+            background.Controls.Add(candy1);
+            candy1.BackColor = Color.Transparent;
+
+
+        }
+        int puncte = 0, vieti = 3,viteza=5;
+        bool stop = true;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //miscare bomboana
+            foreach (Control x in background.Controls)
+            {
+                if ((string)x.Tag == "candy")
+                {
+                    if (x.Top <= background.Height)
+                    {
+                        x.Top = x.Top + viteza;
+                    }
+                    else
+                    {
+                        Random nr = new Random();
+                        int ya = nr.Next(80, 120);//coordonata pe verticala
+                        int xa = nr.Next(425);//coordonata pe orizontala a bomboanei
+                        x.Top = -ya;
+                        x.Left = xa;
+                    }
+                }
+                
+            }
+            lbl_vieti.Text = Convert.ToString(vieti);
+            foreach (Control x in background.Controls)
+            {
+
+                if ((string)x.Tag == "candy")
+                {
+                    if (wood.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        lbl_vieti.Text = Convert.ToString(vieti);
+                        vieti--;
+                        Random nr = new Random();
+                        int ya = nr.Next(80, 120);//coordonata pe verticala
+                        int xa = nr.Next(425);//coordonata pe orizontala a bomboanei
+                        x.Top = -ya;
+                        x.Left = xa;
+                    }
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        puncte++;
+                        lbl_puncte.Text = Convert.ToString(puncte);
+                        if (puncte % 3 == 0)
+                            viteza += 3;
+                        sunet.Play();
+
+                        Random nr = new Random();
+                        int ya = nr.Next(80, 120);//coordonata pe verticala
+                        int xa = nr.Next(425);//coordonata pe orizontala a bomboanei
+                        x.Top = -ya;
+                        x.Left = xa;
+                    }
+                }
+                if (vieti == 0)
+                {
+                    
+                    stop = false;
+                    timer1.Enabled = false;
+                    this.Hide();
+                    Final f = new Final();
+                    
+                    Program.scor = puncte;
+                    if (Program.scor > Program.max_scor)
+                        Program.max_scor = Program.scor;
+                    f.scor =Program.scor;
+                    f.label1.Text = Convert.ToString(f.scor);
+                    f.record.Text = Convert.ToString(Program.max_scor);
+                    f.ShowDialog();
+                }
+            }
+
+
+        }
+
+        private void background_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (stop == true)
+            {
+                Point position = e.Location;
+                double pX = position.X;
+                player.Left = (int)pX - (player.Width / 2);
+            }
+        }
+    }
+}
